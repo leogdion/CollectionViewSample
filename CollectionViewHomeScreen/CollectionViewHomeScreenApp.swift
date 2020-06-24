a829ccb6afe560608a0a05e2ab25f93a81ba5a6c
Leo Dion <leogdion@brightdigit.com>
2020-06-24 10:05:29 -0400

    added working mock up

---
+++ CollectionViewHomeScreen/CollectionViewHomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewHomeScreenApp.swift
+//  CollectionViewHomeScreen
+//
+//  Created by Leo Dion on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct CollectionViewHomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
